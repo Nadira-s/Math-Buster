fe2953634cb09e89167a0434d590b0fee59edbd3
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2025-10-03 12:19:24 +0500

    Initial Commit

---
+++ Math Buster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math Buster
+//
+//  Created by Nadira Seitkazy  on 03.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Math BusterTests/Math_BusterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Math_BusterTests.swift
+//  Math BusterTests
+//
+//  Created by Nadira Seitkazy  on 03.10.2025.
+//
+
+import Testing
+@testable import Math_Buster
+
+struct Math_BusterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
